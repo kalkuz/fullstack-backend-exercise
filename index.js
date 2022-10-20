72544d755a52ce8338b1831cd5763a94999d3796
Oğuzhan Kalkuz <kalkuzoguzhann@gmail.com>
2022-10-20 16:08:23 +0300

    updates with part 5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express, { json } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import bcryptjs from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import jwt, { decode } from 'jsonwebtoken';
 import db from './db.js';
 import routes from './routes/index.js';
 import { Config, Logger, Response } from './utils/index.js';
@@ -10,18 +10,11 @@ import { Person, User } from './models/index.js';
 import { Auth, UnknownEndpoint } from './utils/Middleware.js';
 
 const App = express();
+
 App.use(json({ limit: '100kb' }))
-  .use(cors())
-  .use('/api', routes);
+  .use(cors());
 
-morgan.token('body', (req) => (req.method === 'POST' ? JSON.stringify(req.body) : ''));
-App.use(morgan(':method :url :status - :response-time ms :body'));
-
-App.get('/', (request, response) => {
-  response.send('<h1>Exercise API</h1>');
-});
-
-App.post('/', async (req, res) => {
+App.post('/api/login', async (req, res) => {
   const { username, password } = req.body;
 
   const user = await User.findOne({ username });
@@ -31,22 +24,53 @@ App.post('/', async (req, res) => {
 
   if (!(user && passwordCorrect)) {
     Response.error(res, 'invalid username or password', 'invalid username or password');
+  } else {
+    const userForToken = {
+      usr: user._id,
+      iat: new Date().getTime(),
+      exp: new Date().getTime() + (60 * 60 * 24 * 365 * 1000),
+    };
+
+    const token = jwt.sign(userForToken, Config.JWT_SECRET);
+
+    Response.success(res, { token, username: user.username, name: user.name }, 'Success');
   }
+});
 
-  const userForToken = {
-    usr: user._id,
-    iat: new Date().getTime(),
-    exp: new Date().getTime() + (60 * 60 * 24 * 365 * 1000),
-  };
-
-  const token = jwt.sign(userForToken, Config.JWT_SECRET);
-
-  Response.success(res, { token, username: user.username, name: user.name }, 'Success');
+App.get('/api/me', async (req, res) => {
+  const token = req.headers.authorization || req.query.token;
+  if (token) {
+    try {
+      const { usr, exp } = decode(
+        // token query can be used for web services to be hooked.
+        token,
+        Config.JWT_SECRET,
+      );
+      if (exp > new Date().getTime()) {
+        User.findOne({ _id: usr })
+          .then((data) => {
+            Response.success(res, data.toJSON(), 'Can');
+          })
+          .catch(() => res.status(401).json({ message: 'No such user registered.' }));
+      } else res.status(401).send('Expired token');
+    } catch (error) { res.status(401).send('You need a token.'); }
+  } else {
+    Response.error(res, 'You do not exist', 'You do not exist');
+  }
 });
 
 App.use(Auth('all'));
 
-App.get('/info', (request, response, next) => {
+App.use('/api', routes);
+
+morgan.token('body', (req) => (req.method === 'POST' ? JSON.stringify(req.body) : ''));
+App.use(morgan(':method :url :status - :response-time ms :body'));
+
+App.get('/', (request, response) => {
+  response.send('<h1>Exercise API</h1>');
+});
+
+App.get('/api/info', (request, response, next) => {
   Person.countDocuments()
     .then((data) => response.send(`Phonebook has info for ${data} people`))
     .catch((err) => next(err));
